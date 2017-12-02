d73a0c9427b8ce8366ff84912a63a717485998f5
Jamie Peeso <jamie.peeso@gmail.com>
2017-12-02 15:20:01 -0500

    bootstrap and portfolio page added

---
+++ js/main.js
@@ -0,0 +1,63 @@
+$(document).ready(function(){
+    // Make request to Github
+    $.ajax({
+      url: 'https://api.github.com/users/jamiepeeso',
+      data: {
+        client_id: 'fdfd3cff98adf96f5456',
+        client_secret: 'a970bcc5d8a00570c0400f83dba3b1a6e9af75a9'
+      }
+    }).done(function(user){
+      $.ajax({
+        url: 'https://api.github.com/users/jamiepeeso/repos',
+        data: {
+          client_id: 'fdfd3cff98adf96f5456',
+          client_secret: 'a970bcc5d8a00570c0400f83dba3b1a6e9af75a9',
+          sort: 'created: asc',
+          per_page: 5
+        }
+      }).done(function(repos){
+        $.each(repos, function(index, repo){
+          $('#repos').append(`
+            <div class="well">
+              <div class="row"> 
+                <div class="col-md-3">
+                  <img src="${repo.html_url + 'somepathelements/images/screenshot.png?raw=true' }" /></div>
+                </div>
+                <div class="col-md-7">
+                  <strong>${repo.name}</strong>: ${repo.description} Last Updated: ${repo.updated_at}
+                </div>
+                <div class="col-md-2">
+                  <a href="${repo.html_url}" target="_blank" class="btn btn-secondary btn-block">Repo Page</a>
+                  <br><br>
+                </div>
+              </div>
+            </div>
+            `);
+        });
+      });
+      $('#profile').html(`
+        <div class="card">
+          <h4 class="card-header">${user.name}</h4>
+          <div class="card-body">
+            <div class="row">
+              <div class="col-md-3">
+                <img class="thumbnail avatar" src="${user.avatar_url}">
+                <br><br>
+                <a target="_blank" href="${user.html_url}" class="btn btn-secondary btn-block">View Profile</a>
+              </div>
+              <div class="col-md-9">
+              <button type="button" class="btn btn-outline-secondary">Public Repos: ${user.public_repos}</button>   
+              <br><br>
+              <ul class="list-group">
+                <li class="list-group-item">Location: ${user.location}</li>
+                <li class="list-group-item">Member Since: ${user.created_at}</li>
+              </ul>
+              </div>    
+            </div>
+          </div>
+        </div>
+        <h3 class="page-header">Latest Repos</h3>
+        <div id="repos"></div>
+      `);
+    });
+});
